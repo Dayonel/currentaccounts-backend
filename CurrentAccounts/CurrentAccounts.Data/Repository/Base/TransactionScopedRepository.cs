98dc88cb96517a51656a4078e8fbb234afe2b145
Nestor Orest Plysyuk <dayonel@hotmail.es>
2020-08-23 01:40:23 +0200

    Added scoped transaction base repository. (#7)

---
+++ CurrentAccounts/CurrentAccounts.Data/Repository/Base/TransactionScopedRepository.cs
@@ -0,0 +1,38 @@
+﻿using CurrentAccounts.Core.Interfaces.Repository.Base;
+using System;
+using System.Threading.Tasks;
+
+namespace CurrentAccounts.Data.Repository.Base
+{
+    public class TransactionScopedRepository : ITransactionScopedRepository
+    {
+        private readonly CurrentAccountsDbContext _dbContext;
+        public TransactionScopedRepository(CurrentAccountsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> ExecuteScoped(Func<Task<bool>> function)
+        {
+            using (var transaction = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    if (!await function())
+                    {
+                        await transaction.RollbackAsync();
+                        return false;
+                    }
+
+                    await transaction.CommitAsync();
+                    return true;
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    throw;
+                }
+            }
+        }
+    }
+}
