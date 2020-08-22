1f0eb4f76500a0408b9a894f17ccd49ce2fa0106
Dayonel <dayonel@hotmail.es>
2020-08-22 19:53:29 +0200

    Designed database schema.

---
+++ CurrentAccounts/CurrentAccounts.Core/Entity/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using CurrentAccounts.Core.Entity.Base;
+using CurrentAccounts.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace CurrentAccounts.Core.Entity
+{
+    public class BankAccount : EntityBase, ICreated
+    {
+        public decimal Balance { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public virtual int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual List<Transaction> Transactions { get; set; }
+    }
+}
---
+++ CurrentAccounts/CurrentAccounts.Data/DbMapping/BankAccountMap.cs
@@ -0,0 +1,24 @@
+﻿using CurrentAccounts.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CurrentAccounts.Data.DbMapping
+{
+    public static class BankAccountMap
+    {
+        public static ModelBuilder MapBankAccounts(this ModelBuilder modelBuilder)
+        {
+            EntityTypeBuilder<BankAccount> entity = modelBuilder.Entity<BankAccount>();
+
+            // Primary Key
+            entity.HasKey(t => t.Id);
+
+            // Relations
+            entity.HasMany(p => p.Transactions)
+                  .WithOne(o => o.BankAccount)
+                  .HasForeignKey(f => f.BankAccountId);
+
+            return modelBuilder;
+        }
+    }
+}
