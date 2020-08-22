e928159d55d217194083d2d316b8925f045774f1
Dayonel <dayonel@hotmail.es>
2020-08-23 01:39:55 +0200

    Added scoped transaction base repository.

---
+++ CurrentAccounts/CurrentAccounts.Core/Interfaces/Repository/Base/ITransactionScopedRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CurrentAccounts.Core.Interfaces.Repository.Base
+{
+    public interface ITransactionScopedRepository
+    {
+        Task<bool> ExecuteScoped(Func<Task<bool>> function);
+    }
+}
