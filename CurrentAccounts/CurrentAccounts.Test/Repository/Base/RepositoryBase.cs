da59d1e714b1f0907269805ad5f4888f50382a9e
Dayonel <dayonel@hotmail.es>
2020-08-24 01:47:36 +0200

    Added unit tests.

---
+++ CurrentAccounts/CurrentAccounts.Test/Repository/Base/RepositoryBase.cs
@@ -0,0 +1,29 @@
+﻿using CurrentAccounts.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CurrentAccounts.Test.Repository.Base
+{
+    public abstract class RepositoryBase : IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+        protected readonly CurrentAccountsDbContext _dbContext;
+        protected RepositoryBase()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<CurrentAccountsDbContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+            _dbContext = new CurrentAccountsDbContext(options);
+            _dbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+    }
+}
